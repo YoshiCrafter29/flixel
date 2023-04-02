19014dc98159ff386ddb9a49c85cf5e420902ac9
Yoshman29 <81191621+YoshiCrafter29@users.noreply.github.com>
2023-04-02 15:06:06 +0200

    Revert "high dpi?"
    This reverts commit f7e39cbbdbbdb336be068323a5da3a4f21ac93e9.

--- flixel/FlxG.hx
+++ flixel/FlxG.hx
@@ -369,7 +369,7 @@ class FlxG
 	 */
 	public static inline function resizeGame(Width:Int, Height:Int):Void
 	{
-		scaleMode.onMeasure(Std.int(Width * stage.window.scale), Std.int(Height * stage.window.scale));
+		scaleMode.onMeasure(Width, Height);
 	}
 
 	/**
